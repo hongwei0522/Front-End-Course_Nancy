995931528937c6e90ec1513d5711eedb7ba91fb1
Aria <112182ssss@gmail.com>
2025-02-15 13:29:02 +0800

    page1-part3

---
+++ students/Nancy/Front-Enter/toTop.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTopButton = document.getElementById("backToTop");
+    backToTopButton.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
